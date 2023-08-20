44ea4883ae583daed3d07717533b03c8c5149c37
Sayhighz <sayhixdd@gmail.com>
2023-08-20 12:30:47 +0700

    Add Command !s
    !s (nameserver) will getting player name,duration and show in discord

---
+++ bot.py
@@ -0,0 +1,146 @@
+import discord
+import asyncio
+import a2s
+import requests
+import config
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+intents = discord.Intents.all()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+
+async def get_steam_id_from_custom_url(custom_url):
+    try:
+        response = requests.get(
+            f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={config.STEAM_API_KEY}&vanityurl={custom_url}')
+        response_json = response.json()
+        if response_json['response']['success'] == 1:
+            return response_json['response']['steamid']
+    except:
+        return None
+
+
+async def showServer(serverName):
+    try:
+        destination = requests.get(
+            f"https://www.battlemetrics.com/servers/ark?q={serverName}&sort=score")
+        soup = BeautifulSoup(destination.content, 'html.parser')
+
+        all_servername_raw = soup.find_all("a")
+        all_server_list = {}
+
+        for serverList in all_servername_raw:
+            if serverList.get("title"):
+                for numberServer in range(1, 11):
+                    all_server_list[numberServer] = [
+                        (serverList.get("title"), serverList.get("href"))]
+        return all_server_list
+    except:
+        pass
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    # show activity bot
+    activity = discord.Activity(
+        name='Eye of Sayhigh', type=discord.ActivityType.playing)
+    await client.change_presence(activity=activity)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # funcion show player name and time on server
+    if message.content.startswith('!s'):
+        serverName = message.content.split()[1]
+    try:
+        # show server from battlemetrics
+        destination = requests.get(
+            f"https://www.battlemetrics.com/servers/ark?q={serverName}&sort=score")
+        soup = BeautifulSoup(destination.content, 'html.parser')
+
+        all_servername_raw = soup.find_all("a")
+        global all_server_list
+        all_server_list = {}
+        list_number = 0
+
+        for serverList in all_servername_raw:
+            if serverList.get("title"):
+                list_number += 1
+                all_server_list[list_number] = [
+                    (serverList.get("title"), serverList.get("href"))]
+                
+        server_list = ("""```## Multiple servers found. Specify the server name or enter a number.
+                       
+                       """)
+        
+        for number, name in all_server_list.items():
+            server_list += (f"\n({number}) - { name[0][0]}")
+        await message.channel.send(server_list+"```")
+    except:
+        pass
+
+    try:
+        # wait message from user
+        response = await client.wait_for(
+            'message',
+            timeout=15,
+            check=lambda m: m.author == message.author
+        )
+        server_selection = int(response.content)
+    except asyncio.TimeoutError:
+        print("time out")
+
+    try:
+        # get server IPAddress from user selected server
+        server = all_server_list[server_selection]
+        destination = requests.get(
+            f"https://www.battlemetrics.com{server[0][1]}")
+        soup = BeautifulSoup(destination.content, 'html.parser')
+        find_ip = soup.find(class_="css-1i1egz4")
+
+        data = []
+
+        for element in find_ip:
+            span_elements = element.find_all("span")
+            for span in span_elements:
+                data.append(span.get_text())
+        ipAddress = data[1]
+        server_ip = ipAddress.split(":")
+        server_ip = (server_ip[0], int(server_ip[1]))
+    except UnboundLocalError:
+        pass
+
+    try:
+        players = a2s.players(address=server_ip)
+        server_info = a2s.info(address=server_ip)
+        await message.channel.send(f"```{server_info.server_name}\nMap: {server_info.map_name}\nPlayers: {server_info.player_count}/{server_info.max_players}```")
+        player_list = []
+        # add player data to player_list
+        for player in players:
+            player_name = player.name
+            player_duration = player.duration
+            hours, remainder = divmod(player_duration, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            time_str = f"{int(hours)}::{int(minutes)}::{int(seconds)}"
+            formatted_duration = datetime.strptime(
+                time_str, '%H::%M::%S').time()
+
+            player_list.append((player_name, formatted_duration))
+        show_player_list = "``` Online Time:          Name:"
+        for name, duration in player_list:
+            show_player_list += (f"\n  [{duration}]           {name}")
+
+        await message.channel.send(show_player_list+"```")
+    except Exception as e:
+        print(e)
+
+client.run(config.BOT_KEY)
