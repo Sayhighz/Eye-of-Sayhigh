3aecf8bd06e2e11e73c8f9b34d1a48388dc92dca
Sayhighz <sayhixdd@gmail.com>
2023-08-21 10:48:22 +0700

    add run file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_KEY = "your_bot_token" # replace your bot token
+BOT_KEY = "your bot_token" # replace your bot token
 
-STEAM_API_KEY = "your_steam_api_key" # replace your steam api key
+STEAM_API_KEY = "your steam_api_key" # replace your steam api key
